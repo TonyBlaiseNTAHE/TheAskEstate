3e9bdf228029a2c3f4842cd96f7d23a59c620b5b
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-09-05 09:27:50 +0200

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongose from "mongoose";
+
+const userSchema = new mongose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongose.model("User", userSchema);
+
+export default User;
